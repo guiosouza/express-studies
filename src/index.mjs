09aa2a767ef490d8fe86708b5bb40758547f082b
unknown <guilhermedeoliveiradesouza@gmail.com>
2024-09-27 11:38:42 -0300

    cookies

--- src/index.mjs
+++ src/index.mjs
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import routes from "./routes/index.mjs";
+import cookieParser from "cookie-parser";
 
 // loading .env
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser('helloWorld'));
 app.use(routes);
 
 const PORT = process.env.PORT || 5555;
@@ -16,5 +18,6 @@ app.listen(PORT, () => {
 });
 
 app.get("/", (request, response) => {
+  response.cookie("hello", "world", { maxAge: 30000, signed: true });
   response.status(201).send({ message: "Hello there" });
 });
--- src/routes/users.mjs
+++ src/routes/users.mjs
@@ -4,11 +4,20 @@ const router = Router();
 
 // GET ALL USERS
 router.get("/api/users", (request, response) => {
-  response.send([
-    { id: 1, username: "guios", email: "guilherme@gmail.com" },
-    { id: 2, username: "aldivio", email: "aldivio@gmail.com" },
-    { id: 3, username: "gus", email: "gustavo@gmail.com" },
-  ]);
+
+  console.log(request.headers.cookie);
+  console.log(request.cookies);
+  console.log("request.signedCookies.hello: ", request.signedCookies.hello);
+
+  if ((request.signedCookies.hello && request.signedCookies.hello === "world")) {
+    return response.send([
+      { id: 1, username: "guios", email: "guilherme@gmail.com" },
+      { id: 2, username: "aldivio", email: "aldivio@gmail.com" },
+      { id: 3, username: "gus", email: "gustavo@gmail.com" },
+    ]);
+  }
+
+  return response.status(403).send({ message: "Sorry, you need the correct cookie" });
 });
 
 export default router;
